e356bf8a352140cb3c63b6934a512345d9fb89ce
Oisamoje Victor <>
2023-01-05 10:27:19 +0100

    update

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 4400;
-app.get('/', (req, res) => {
-    res.send('stock_market_prediction');
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("stock_market_prediction");
 });
 app.listen(port, () => {
-    console.log(`app is been listen to on ${port}`);
+    console.log(`app is been listen to on http://localhost:${port}`);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = 4400;
+const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('stock_market_prediction');
+app.get("/", (req: Request, res: Response) => {
+  res.send("stock_market_prediction");
 });
 app.listen(port, () => {
-  console.log(`app is been listen to on ${port}`);
+  console.log(`app is been listen to on http://localhost:${port}`);
 });
