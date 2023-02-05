ada50e6a1e0b8a4778dd4563200c6bf64200fa2b
Oisamoje Victor <>
2023-02-05 20:34:41 +0100

    updated

--- server/config/default.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-require("../utils/dotEnv");
-module.exports = {
-    PORT: process.env.PORT || '1000'
-};
---
+++ server/utils/errorMessage.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    StockNotFound: "Stock was not found",
+    StockPrediction: "Stock prediction Not Successful"
+};
