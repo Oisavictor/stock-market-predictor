86ba388576f52454fe1ae077f9508bd2263a990c
Oisamoje Victor <>
2023-02-05 20:47:03 +0100

    Changed port to 4000

--- server/Interface/user.interface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserPrisma = void 0;
-const client_1 = require("@prisma/client");
-exports.UserPrisma = new client_1.PrismaClient();
--- server/utils/errorMessage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    StockNotFound: "Stock was not found",
-    StockPrediction: "Stock prediction Not Successful"
-};
