87e9161235bdd296659990ba0b2a775dc2acbaad
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2023-02-10 15:42:54 +0100

    Update package.json

--- server/controller/Sales.controller.ts
+++ server/controller/Sales.controller.ts
@@ -1,8 +1,1 @@
 // import * as express from 'express';
-import { sellingPrice } from '../services/greet.service';
-
-export const postDiscount = async (req, res) => {
-    const { sales } = req.body;
-    const discount = await sellingPrice(sales);
-    return res.status(200).json(discount);
-};
