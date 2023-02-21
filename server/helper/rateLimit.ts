5ac8d3deec5c8e99905cd3442ae1a9813dbd28d9
InvestVision Inc <stockprediction23@gmail.comm>
2023-02-21 13:45:48 +0100

    kjs

--- server/helper/rateLimit.ts
+++ server/helper/rateLimit.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as express from 'express';
 import { rateLimit } from 'express-rate-limit';
 
@@ -7,11 +8,26 @@ const app = express();
 export const apiLimiter = rateLimit ({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+=======
+import * as express from "express";
+const app = express();
+import rateLimit from 'express-rate-limit';
+
+
+
+export const apiLimiter = rateLimit ({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+>>>>>>> 6904fb3e8e7d7518982e911908961f8d19ce9d97
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+});
 
+<<<<<<< HEAD
 app.use('/api/', apiLimiter);
+=======
+app.use(apiLimiter);
+>>>>>>> 6904fb3e8e7d7518982e911908961f8d19ce9d97
 
 // export const createAccountLimiter = rateLimit({
 // 	windowMs: 60 * 60 * 1000, // 1 hour
@@ -22,6 +38,6 @@ app.use('/api/', apiLimiter);
 // 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 // })
 
-// app.post('/create-account', createAccountLimiter, (request, response) => {
-// 	//...
-// })
+// // app.post('/create-account', createAccountLimiter, (request, response) => {
+// // 	//...
+// // })
--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,5 +1,9 @@
 import * as express from "express";
+<<<<<<< HEAD
 import { apiLimiter } from "../helper/rateLimit"
+=======
+import { apiLimiter } from "../helper/rateLimit";
+>>>>>>> 6904fb3e8e7d7518982e911908961f8d19ce9d97
 import { validateResource } from "../resources/validateResources";
 import {
   createUserController,
@@ -12,7 +16,11 @@ export const UserRoutes = (router: any) => {
   router.post(
     "/api/user",
     validateResource(createUserSchema),
+<<<<<<< HEAD
     createUserController
+=======
+    createUserController,
+>>>>>>> 6904fb3e8e7d7518982e911908961f8d19ce9d97
   );
 
   router.post(
