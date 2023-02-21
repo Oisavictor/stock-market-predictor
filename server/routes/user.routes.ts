1b99d4af49d3f1e42a65f33a160f125574991b36
InvestVision <stockprediction23@gmail.com>
2023-02-21 14:41:36 +0100

    Update user.routes.ts

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -8,6 +8,13 @@ import {
 import { getStockPrice } from "../controller/StockPriceController";
 import { createUserSchema, verifyUserOTPSchema } from "../schema/user.schema";
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message:
+    "Too many requests from this IP, please try again after 15 minutes"
+});
+
 export const UserRoutes = (router: any) => {
   router.post(
     "/api/user",
