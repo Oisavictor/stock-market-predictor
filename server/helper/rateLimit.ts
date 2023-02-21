0e94e67594d6b05fc05e8b205c340a895f1d7eb3
InvestVision Inc <stockprediction23@gmail.comm>
2023-02-21 12:30:28 +0100

    first commit

--- server/helper/rateLimit.ts
+++ server/helper/rateLimit.ts
@@ -1,13 +1,17 @@
+import * as express from 'express';
 import rateLimit from 'express-rate-limit';
 
-export const apiLimiter = rateLimit({
+const app = express();
+
+
+export const apiLimiter = rateLimit ({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
-// app.use('/api/', apiLimiter)
+app.use('/api/', apiLimiter);
 
 export const createAccountLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 1 hour
