891d337b4fd9e10876b2ba99f18f5c2df426007f
InvestVision Inc <stockprediction23@gmail.comm>
2023-02-21 12:30:48 +0100

    Start

--- server/helper/rateLimit.ts
+++ server/helper/rateLimit.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import rateLimit from 'express-rate-limit';
+import { rateLimit } from 'express-rate-limit';
 
 const app = express();
 
@@ -13,14 +13,14 @@ export const apiLimiter = rateLimit ({
 
 app.use('/api/', apiLimiter);
 
-export const createAccountLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000, // 1 hour
-	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
-	message:
-		'Too many accounts created from this IP, please try again after an hour',
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+// export const createAccountLimiter = rateLimit({
+// 	windowMs: 60 * 60 * 1000, // 1 hour
+// 	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+// 	message:
+// 		'Too many accounts created from this IP, please try again after an hour',
+// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// })
 
 // app.post('/create-account', createAccountLimiter, (request, response) => {
 // 	//...
