3088034508cae30e78f0bf58db831da248481baa
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2023-02-23 15:14:35 +0100

    Update express.ts

--- server/express.ts
+++ server/express.ts
@@ -10,7 +10,16 @@ import {UserRoutes} from './routes/user.routes'
 //import prisma to connect automatically
 import { connectPrisma } from "./connectPrisma";
 //Express connection  
-import { apiLimiter } from "./helper/rateLimit"
+import rateLimit from 'express-rate-limit'
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to API calls only
 export const ExpressConnection = async() => {
 
     const PORT = config.get<number>('PORT')
