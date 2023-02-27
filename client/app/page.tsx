136a247c9997add9c067fb24ab69572c5f26edaa
Davidson <davidsonakra@gmail.com>
2023-02-27 12:17:43 +0100

    created hero component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from './page.module.css'
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className='h-100vl'>
-      <p className='bg-black text-5xl font-bold '>the body</p>
+    <main  className='h-screen bg-white'>
+      <Hero/>
     </main>
   )
 }
