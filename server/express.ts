2badc94e840ae8e024bfb3f9c2aae1f5c714aee0
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2023-02-28 16:20:06 +0100

    Fix the csrf bug issue

--- server/express.ts
+++ server/express.ts
@@ -1,48 +1,54 @@
 //All Dev dependency are called her
 import * as express from "express";
-import * as cors from 'cors'
-import * as config from 'config'
-import * as cookieParser from 'cookie-parser'
+import * as cors from "cors";
+import * as config from "config";
+import * as cookieParser from "cookie-parser";
+import * as lusca from "lusca";
+import helmet from "helmet";
 //Logger is called here
 import { logger } from "./middleware/logger";
 //All routes file is called here
-import {Routes} from "./routes/route";
-import {UserRoutes} from './routes/user.routes'
+import { Routes } from "./routes/route";
+import { UserRoutes } from "./routes/user.routes";
 //import prisma to connect automatically
 import { connectPrisma } from "./connectPrisma";
-//Express connection  
-import rateLimit from 'express-rate-limit'
+//Express connection
+import rateLimit from "express-rate-limit";
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Apply the rate limiting middleware to API calls only
-export const ExpressConnection = async() => {
+export const ExpressConnection = async () => {
+  const PORT = config.get<number>("PORT");
+  const accessToken = config.get<string>("token.ACCESS_TOKEN");
+  const app = express();
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "http://localhost:4200",
+      ],
+      credentials: true,
+    })
+  );
+  // app.use('/', router);
+  app.use(helmet());
+  app.use(lusca.csrf());
+  await connectPrisma();
+  app.use("/api", apiLimiter);
+  Routes(app);
+  UserRoutes(app);
 
-    const PORT = config.get<number>('PORT')
-    const app = express()
-    app.use(express.json());
-    app.use(cookieParser())
-    app.use(cors({
-      origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200'],
-      credentials: true
-    }))
-    // app.use('/', router);
-    await connectPrisma()
-
-    app.use('/api', apiLimiter)
-    Routes(app)
-    UserRoutes(app)
-    
-    app.listen(PORT, () => {
-      logger.info(`app is been listen to on http://localhost:${PORT}`)
-      }
-    )
-    
-} 
-ExpressConnection()
-
+  app.listen(PORT, () => {
+    logger.info(`app is been listen to on http://localhost:${PORT}`);
+  });
+};
+ExpressConnection();
