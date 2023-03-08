8e2a08bfa8cb75b1fc0578dc0a83159f0933083f
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2023-03-08 13:07:17 +0100

    solve ssl issue

--- server/config/email.config.ts
+++ server/config/email.config.ts
@@ -2,15 +2,13 @@ import * as config from 'config';
 import { createTransport } from 'nodemailer';
 
 const host = config.get<string>('mailgun.HOST');
-const port = config.get<number>('mailgun.PORT');
-const secure = config.get<boolean>('mailgun.SECURE');
 const user = config.get<string>('mailgun.USER');
 const pass = config.get<string>('mailgun.PASS');
 
 export const mail = createTransport({
     host: host,
-    port: port,
-    secure: secure,
+    port: 587,
+    secure: false,
     auth: {
         user: user,
         pass: pass
