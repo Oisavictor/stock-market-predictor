1f7d31f7cd75ece4f9d139638f4f89518c19e7f1
InvestVision Inc <stockprediction23@gmail.com>
2023-03-29 21:28:31 +0100

    fix

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className='flex flex-row justify-between px-10 h-screen'>
       <div className='w-1/2 flex justify-center flex-col'>
-        <h1 className='text-black text-8xl font-bold'>A smart tool for <span className='text-blue-600'>stock market</span> analysis</h1>
+        <h1 className='text-black text-6xl font-bold'>A smart tool for <span className='text-blue-600'>stock market</span> analysis</h1>
         <p className='font-light text-xl mt-5'>Predict the market movements for smarter investing</p>
         <div className='flex mt-10'>
           <button className='border-2 border-blue-600 flex justify-center items-center px-10 py-2 mr-3 rounded-md font-bold text-black'>View all features</button>
